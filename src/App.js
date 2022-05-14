829bd75ba94f99b38d02ea97b58bde44f7551e21
Sven Dudink <sven@dudink.net>
2022-05-14 11:17:33 +0200

    testing code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { serial } from "./helper/serial";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={() => {
+          serial();
+          console.log("testrun5");
+        }}
+        style={{ position: "fixed", zIndex: "1000", paddingTop: "60px" }}
+      >
+        Add
+      </button>
     </div>
   );
 }
